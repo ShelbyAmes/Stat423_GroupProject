f4bd9af5f47470c0900322ab8d158d48bac93ba3
Shelby Ames <shelbyames10@gmail.com>
2025-02-26 10:34:11 -0800

    Added finished combined data

--- SA_code.R
+++ SA_code.R
@@ -7,6 +7,9 @@ washington <- divorce[49,1:26]
 ##need to unpivot data to pull divorce rates and year in 2 different columns
 library(tidyverse)
 
+washington <- washington[,] %>%
+  mutate(across(everything(), as.character))
+
 wa_long <- washington %>%
   pivot_longer(
     cols = starts_with("X"), 
@@ -21,13 +24,12 @@ ggplot(data = wa_long, aes(x=Year, y=DivorceRate)) +
   geom_point()+
   theme_minimal()
 
-personal_income <- read.csv("SAINC70_WA_2000_2023.csv")
 
-GDP <- read.csv("WA_GDP.csv")
+GDP_percentage <- read.csv("WA_GDP.csv")
 
-WA_total_GDP <- GDP[1,9:34]
+WA_total_GDP_perc <- GDP[1,9:34]
 
-WA_GDP_long <- WA_total_GDP %>%
+WA_GDP_long_perc <- WA_total_GDP %>%
   pivot_longer(
     cols = starts_with("X"), 
     names_to = "Year",        
@@ -35,4 +37,45 @@ WA_GDP_long <- WA_total_GDP %>%
   ) %>%
   mutate(Year = as.integer(str_remove(Year, "X")))
 
-##just seeing if commit works
+WA_expense <- read.csv("SASUMMARY_WA_1998_2023.csv")
+
+WA_expense <- WA_expense[,] %>%
+  mutate(across(everything(), as.character))
+
+personal_income <- WA_expense[5,9:34]
+
+pi_long <- personal_income %>%
+  pivot_longer(
+    cols = starts_with("X"), 
+    names_to = "Year",        
+    values_to = "PersonalIncome" 
+  ) %>%
+  mutate(Year = as.integer(str_remove(Year, "X")))
+
+RealGDP <- WA_expense[1,9:34]
+
+rgdp_long <- RealGDP %>%
+  pivot_longer(
+    cols = starts_with("X"), 
+    names_to = "Year",        
+    values_to = "RealGDP" 
+  ) %>%
+  mutate(Year = as.integer(str_remove(Year, "X")))
+
+Expenditures <- WA_expense[7,9:34]
+
+expenditures_long <- Expenditures %>%
+  pivot_longer(
+    cols = starts_with("X"), 
+    names_to = "Year",        
+    values_to = "PersonalExpenditures" 
+  ) %>%
+  mutate(Year = as.integer(str_remove(Year, "X")))
+
+
+wa_combined <- wa_long %>%
+  left_join(WA_GDP_long_perc, by = "Year") %>%
+  left_join(pi_long, by = "Year") %>%
+  left_join(rgdp_long, by = "Year") %>%
+  left_join(expenditures_long, by = "Year")
+
